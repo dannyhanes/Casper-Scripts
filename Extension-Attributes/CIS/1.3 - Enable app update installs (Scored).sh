3d90e941671127ad03f33f0257cec4f6a0158727
Danny Hanes <guardianfx@gmail.com>
2016-06-01 14:39:56 -0400

    Update 1.3 - Enable app update installs (Scored).sh

--- Extension-Attributes/CIS/1.3 - Enable app update installs (Scored).sh
+++ Extension-Attributes/CIS/1.3 - Enable app update installs (Scored).sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+#Ensure that application updates are installed after they are available from Apple. 
+#These updates do not require reboots or admin privileges for end users.
+
 RESULT=defaults read /Library/Preferences/com.apple.commerce AutoUpdate
 
 echo "<result>$RESULT</result>"
